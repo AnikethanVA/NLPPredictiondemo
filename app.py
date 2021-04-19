48c6f70a289dfa69682f90a0413f2b72d6813a95
Anikethan <anikethan.va@gmail.com>
2021-04-19 17:49:48 +0530

    first commit-added changes to app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import tkinter as tk
 from PIL import Image, ImageTk
 from tkinter.filedialog import askopenfile
-
+import predict
 
 root = tk.Tk()
 root.title("Word Predictor")
@@ -26,18 +26,28 @@ usrinp.grid(columnspan=5, row=2, rowspan=1)
 
 #browse button
 browse_text = tk.StringVar()
-browse_btn = tk.Button(root, textvariable=browse_text, font="Raleway", bg="#20bebe", fg="white", height=2, width=15)
+browse_btn = tk.Button(root, textvariable=browse_text, font="Raleway", bg="#20bebe", fg="white", height=2, width=15,command=lambda:onclick(usrinp.get()))
 browse_text.set("Predict")
 browse_btn.grid(column=1, row=3)
 
+#onclick
+def onclick(text):
+    res=predict.Enter_Text(text)
+    Suggestion['text'] =res
+
+
 #label
-# output = tk.Label(root, "Checking")
-# output.grid
+output = tk.Label(root,text="Suggestions:",font="Helvetica", background='#FFDE59')
+output.grid(column=1,row=4)
+
+#Suggestion Label
+Suggestion=tk.Label(root,text="",font="Helvetica",foreground='#5E17EB',background='#FFDE59')
+Suggestion.grid(row=5,columnspan=5)
 
 #titlebar ico
-ico = Image.open('ico.png')
-ico = ImageTk.PhotoImage(ico)
-root.iconphoto(False, ico)
+# ico = Image.open('ico.png')
+# ico = ImageTk.PhotoImage(ico)
+# root.iconphoto(False, ico)
 
-root.resizable(False, False)
+# root.resizable(False, False)
 root.mainloop()
